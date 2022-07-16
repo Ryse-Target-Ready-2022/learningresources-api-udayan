8c85a567d25f6d576c30bb860ef0703506f9644a
udayanChaudhary <udayanchaudhary2000@gmail.com>
2022-07-16 13:26:49 +0530

    added hello world

---
+++ src/main/java/com/target/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -0,0 +1,14 @@
+package com.target.rysetii.learningresourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiApplication.class, args);
+		System.out.println("Hello World !!!!!");
+	}
+
+}
